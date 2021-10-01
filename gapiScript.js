dc007563b57a47c1cd14de7f25742c7e29332ec6
jonnyparris <ruskin.constant+dev@gmail.com>
2021-10-01 16:33:31 +0100

    nullcheck window

--- gapiScript.js
+++ gapiScript.js
@@ -1,4 +1,4 @@
-var gapi=window.gapi=window.gapi||{};gapi._bs=new Date().getTime();(function(){/*
+var gapi=(window && (window.gapi=window.gapi))||{};gapi._bs=new Date().getTime();(function(){/*
 
   Copyright The Closure Library Authors.
   SPDX-License-Identifier: Apache-2.0
